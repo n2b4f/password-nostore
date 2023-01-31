9633b5bc4ddf3f1d920cf8f59e34f727a3f66df0
vkbubnov <in2b4f@gmail.com>
2023-01-31 18:59:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+import hashlib
+import getpass
+import os
+import argparse
+import json
+import subprocess
+
+class Config:
+    def __init__(self):
+        config_path = os.path.expanduser("~/.config/password-nostore/config")
+        config_dictionary = {}
+        if os.path.isfile(config_path):
+            config_file = open(config_path,"r")
+            for line in config_file:
+                (key, value) = line.split('=')
+                config_dictionary[key.strip()] = value.strip()
+        self.dictionary = config_dictionary
+    
+    def get_default_iteration(self):
+        if "use_default_iterations" in self.dictionary and "default_add_iterations" in self.dictionary:
+            if self.dictionary["default_add_iterations"].isnumeric() \
+                and self.dictionary["use_default_iterations"] in ["y", "Y", "yes", "Yes", "YES", "True", "true", "TRUE"]:
+                return int(self.dictionary["default_add_iterations"])
+        return "none"
+    
+            
+def hashing_password(conc_str: str, iterations: int):
+    hashedpassword_hex = hashlib.sha256(conc_str.encode('utf-8'))
+    for i in range(1, iterations):
+        hashedpassword_hex = hashlib.sha256(hashedpassword_hex.hexdigest().encode('utf-8'))
+    hashedpassword = hashedpassword_hex.hexdigest()
+    return(hashedpassword)
+
+def ask_info_form(account_info: dict):
+    if 'target' not in account_info:
+        account_info['target'] = input('Input target or url: ')
+    if 'username' not in account_info:
+        account_info['username'] = input('Input username: ')
+    if 'additional_iterations' not in account_info:
+        if config.get_default_iteration() != 'none':
+            account_info['additional_iterations'] = config.get_default_iteration()
+        else:
+            input_add_iterations = input('Enter the number of additional iterations (default is 100000): ')
+            if input_add_iterations != '' and input_add_iterations.isnumeric():
+                account_info['additional_iterations'] = int(input_add_iterations)
+            else:
+                account_info['additional_iterations'] = 0
+    password = getpass.getpass('Input password (the input is not visible): ')
+    returned_dict = {'account_info': account_info, 'password': password}
+    return returned_dict
+
+def ask_what_to_save(account_info: dict):
+    what_to_save = input("What from this data you want to save to file? \n [1]target [2]username [3]additional iterations [4]all of them: ")
+    if "4" in what_to_save:
+        return account_info
+        print("вернуть словарь, а иначе накапливать новый словарь и вернуть его")
+    else:
+        save_info = {}
+        if '1' in what_to_save:
+            save_info['target'] = account_info['target']
+        if '2' in what_to_save:
+            save_info['username'] = account_info['username']
+        if '3' in what_to_save:
+            save_info['additional_iterations'] = account_info['additional_iterations']
+        return save_info
+
+def main():
+    account_info = {}
+    file_path = args.file
+    if args.save == True:
+        ask_overwrite = 'Y'
+        if os.path.isfile(file_path):
+            ask_overwrite = input('File already exist. Do you want to overwrite your file?: [y/N] ')
+    else:
+        if os.path.isfile(file_path):
+            with open(file_path) as account_file:
+                account_info = json.loads(account_file.read())
+    result = ask_info_form(account_info)
+    account_info = result['account_info']
+    password = result['password']
+    conc_str  = account_info['target'] + account_info['username'] + password
+    iterations = account_info['additional_iterations'] + def_iterations
+    if args.save == True:
+        account_info_save = ask_what_to_save(account_info)
+        if ask_overwrite in ["y", "Y", "yes", "Yes", "YES", "True", "true", "TRUE"]:
+            if not os.path.isfile(file_path):
+                file_path_list = file_path.split("/")
+                file_path_filename = file_path_list[-1]
+                file_path_list.pop(-1)
+                file_path = './'
+                for folder in file_path_list:
+                    file_path = file_path + folder + '/'
+                os.makedirs(file_path)
+                file_path = file_path + file_path_filename
+            with open(file_path, "w") as account_file:
+                account_file.write(json.dumps(account_info_save))
+        else:
+            print("File not saved")
+    hashed_password = hashing_password(conc_str,iterations)
+    if args.clipboard == False:
+        print(hashed_password)
+    else:
+        import clipboard
+        clipboard.copy(hashed_password)
+        print("Our password copied to clipboard!")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', help='Load file with info', type=str, default='')
+parser.add_argument('-s', '--save', help='Add to storage', action='store_true', default=False)
+parser.add_argument('-c', '--clipboard', help='Copy password to clipboard instead on std::out', action='store_true', default=False)
+args = parser.parse_args()
+config = Config()
+def_iterations = 100000
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,86 @@
+# Password-nostore
+PNS is the idea of a password manager without storing the passwords themselves. The password is obtained by hashing a string consisting of a resource address, a user name and a universal password, that is, there are practically no differences from classic password managers in the execution. The difference is that there is no way to understand directly in the password manager whether the universal password is specified correctly, which can be both a disadvantage and an advantage. Also, the difference is that it's not scary to lose a file with passwords (because there is no such file, but there is something instead of them), it's enough to remember the address, login and universal password. since you have to have different passwords for each site, and there can be many logins, this program can store the address, login and additional hashing iterations (if you need to change the password without changing the universal password) in your home directory.
+
+## How to use
+Just run the program and specify everything you need
+
+    > ./main.py
+    Input target or url: github.com
+    Input username: n2b4f
+    Enter the number of additional iterations (default is 100000): 
+    Input password (the input is not visible): 
+    8238fdaae5b1c6800f0695ddd241a61e5d2234700dfa46fffaefbab0f3626ef2
+
+The hash is the password we wanted to get. It can be used on the website. (the example is not real, don't try it)
+
+### Additional functions
+
+Number of additional iterations - by default, the password is hashed in 100000 iterations, you can enter 1 for 100001 iterations etc.
+
+The number of additional iterations can be specified in the configuration file, which should be in ~/.config/password-nostore/config 
+
+    use_default_iterations = y
+    default_add_iterations = 10
+
+
+Help also avaliable
+
+    usage: main.py [-h] [-f FILE] [-s] [-c]
+
+    options:
+     -h, --help            show this help message and exit
+     -f FILE, --file FILE  Load file with info
+     -s, --save            Add to storage
+     -c, --clipboard       Copy password to clipboard instead on std::out
+
+
+
+The program can store records, usage examples are below
+
+    > ./main.py -f test/test1 -s
+    Input target or url: test.test
+    Input username: someone
+    Enter the number of additional iterations (default is 100000): 2
+    Input password (the input is not visible): 
+    What from this data you want to save to file? 
+     [1]target [2]username [3]additional iterations [4]all of them: 4
+    bf7baa186193156b76a1b98450a6db08c62970d45619a74f32117310397532d4
+    > cat test/test1 
+    {"target": "test.test", "username": "someone", "additional_iterations": 2}
+
+    > ./main.py -f test/test1 -s
+    File already exist. Do you want to overwrite your file?: [y/N] y
+    Input target or url: test.test
+    Input username: someone
+    Enter the number of additional iterations (default is 100000): 3
+    Input password (the input is not visible): 
+    What from this data you want to save to file? 
+    [1]target [2]username [3]additional iterations [4]all of them: 1 2
+    c4110e1d7f3a63d26c5d605ac7b5ac71d17547d395f25c746e0a7456edc55957
+    > cat test/test1 
+    {"target": "test.test", "username": "someone"}
+
+    > ./main.py -f test/test1
+    Enter the number of additional iterations (default is 100000): 3
+    Input password (the input is not visible): 
+    c4110e1d7f3a63d26c5d605ac7b5ac71d17547d395f25c746e0a7456edc55957
+
+    > ./main.py -f test/test1 -c
+    Input password (the input is not visible): 
+    Our password copied to clipboard!
+
+## Installation
+It's simple, everything is done in one file, so it's enough to add it to any directory from where it will be convenient to launch it and set its launch permissions, for example in /usr/local/bin, but I personally recommend adding it to $HOME/.local/bin, and adding this directory to $PATH (depends on your shell: bash, zsh, fish etc).
+
+    > cp main.py ~/.local/bin/pns
+    # and then use
+    > pns -f .password-nostore/myfolders/myfiles
+
+Also need to clipboard
+
+    > pip install clipboard
+
+maybe install other stuff like `sudo apt-get install xclip` or `sudo apt-get install xsel` or `sudo apt install wl-clipboard` I dont know
+
+
+
